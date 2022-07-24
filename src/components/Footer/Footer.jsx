2c9911385f3dc8f420a6693ef7c1e6b590f83c1d
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-07-25 07:33:31 +1200

    add icons to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import "./Footer.css";
+import { IconContext } from "react-icons";
+import { FaGithub, FaFigma } from "react-icons/fa";
+import { GrHomeRounded } from "react-icons/gr";
+import { AiOutlineHome } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="last-line">
         <span>Credits</span>
+        <div className="f-icons">
+          <IconContext.Provider value={{ className: "f-icon" }}>
+            <AiOutlineHome />
+            <a
+              href="https://github.com/ldreaaml/anime-blue-period"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.figma.com/file/1E0JrQiUihnAWFp1ZUPWPH/Blue-Period?node-id=0%3A1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFigma />
+            </a>
+          </IconContext.Provider>
+        </div>
         <span>Design + Code by Dream</span>
       </div>
       <div className="f-background"></div>
