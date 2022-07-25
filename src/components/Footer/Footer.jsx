ae4e3d523d2958a1b61f53409370835d5079de27
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-07-25 22:36:39 +1200

    connect to firebase

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,17 +2,52 @@ import React from "react";
 import "./Footer.css";
 import { IconContext } from "react-icons";
 import { FaGithub, FaFigma } from "react-icons/fa";
-import { GrHomeRounded } from "react-icons/gr";
 import { AiOutlineHome } from "react-icons/ai";
+import { getLikesNumber, incrementLikes } from "../../firebase";
+import { useState } from "react";
+import { initializeApp, ServerValue } from "firebase/app";
+import {
+  getDatabase,
+  ref,
+  child,
+  onValue,
+  push,
+  set,
+  update,
+  increment,
+} from "firebase/database";
+import { useEffect } from "react";
 
 const Footer = () => {
+  const [likesCount, setLikeCounts] = useState(0);
+  const getLikesCount = () => {
+    console.log("get");
+    const db = getDatabase();
+    const LikesCountRef = ref(db, "blue-period/page_likes");
+    onValue(LikesCountRef, (snapshot) => {
+      setLikeCounts(snapshot.val());
+    });
+    console.log(likesCount);
+  };
+
+  useEffect(() => {
+    getLikesCount();
+  }, []);
+
+  const update = () => {
+    incrementLikes();
+  };
   return (
     <div className="footer">
+      <div className="page-stats">
+        <span>This page now has total of {likesCount} Likes!</span>
+      </div>
       <div className="last-line">
         <span>Credits</span>
         <div className="f-icons">
           <IconContext.Provider value={{ className: "f-icon" }}>
-            <AiOutlineHome />
+            <AiOutlineHome onClick={getLikesCount} />
+            <AiOutlineHome onClick={update} />
             <a
               href="https://github.com/ldreaaml/anime-blue-period"
               target="_blank"
---
+++ src/firebase.js
@@ -0,0 +1,47 @@
+import { initializeApp, ServerValue } from "firebase/app";
+import {
+  getDatabase,
+  ref,
+  child,
+  onValue,
+  push,
+  set,
+  update,
+  increment,
+} from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXoG7IOUwaaRobWJGrR3u1-1cZl68QGfY",
+  authDomain: "like-counter-df95f.firebaseapp.com",
+  projectId: "like-counter-df95f",
+  storageBucket: "like-counter-df95f.appspot.com",
+  messagingSenderId: "597105951232",
+  appId: "1:597105951232:web:4fb62efeaec8f2caf8cee5",
+  measurementId: "G-N1P79EQMYK",
+  databaseURL:
+    "https://like-counter-df95f-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const incrementLikes = () => {
+  console.log("update");
+  const db = getDatabase();
+
+  const LikesCountRef = ref(db, "blue-period");
+  update(LikesCountRef, {
+    page_likes: increment(1),
+  });
+};
+
+// export const getLikesNumber = () => {
+//   const [likesCount, setLikeCounts] = useState(null);
+//   console.log("get");
+//   const db = getDatabase();
+//   const LikesCountRef = ref(db, "blue-period/page_likes");
+
+//   const LikesCount = onValue(LikesCountRef, (snapshot) => {
+//     const data = snapshot.val();
+//     return data;
+//   });
+// };
