74d2d5e48e978d250b3a319c69e4d8055d48c0a4
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-07-28 08:20:19 +1200

    style title text on banner

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Intro.css";
 import Banner from "../../img/TempBanner.png";
+import MC from "../../img/Banner_Yatora.jfif";
+import Logo from "../../img/logo.png";
 
 const Intro = () => {
   return (
     <div className="intro" id="Intro">
       <div className="banner">
-        <img src={Banner} alt="" />
+        <div className="yatora">
+          <img src={MC} className="mc" alt="" />
+          <div className="i-title">
+            <span className="title">BLUE</span>
+            <span className="title">PERIOD</span>
+            <img src={Logo} className="i-logo" alt="" />
+          </div>
+
+          <div className="i-author">
+            <span>written by</span>
+            <span>TSUBASA YAMAGUCHI</span>
+          </div>
+        </div>
       </div>
     </div>
   );
