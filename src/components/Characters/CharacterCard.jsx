76b921d6d12c6b323b73426c1f3eda211474fb3d
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-07-29 05:19:34 +1200

    add floating div for character description

--- src/components/Characters/CharacterCard.jsx
+++ src/components/Characters/CharacterCard.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CharacterCard.css";
-const CharacterCard = ({ image, firstName, lastName, color }) => {
+import { motion } from "framer-motion";
+
+const CharacterCard = ({ image, firstName, lastName, color, description }) => {
+  const showDescription = () => {
+    // console.log("test");
+  };
+
+  const [isHovered, setHovered] = useState(false);
+
   return (
-    <div className="card">
+    <div
+      className="card"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
       <div className="c-background" style={{ background: `${color}` }}>
         <div className="c-image">
           <img src={image} alt="" />
@@ -13,6 +25,24 @@ const CharacterCard = ({ image, firstName, lastName, color }) => {
         <br></br>
         <span>{lastName}</span>
       </div>
+
+      <motion.div
+        className="floating-card"
+        initial={false}
+        animate={{
+          left: isHovered ? "80%" : 0,
+          visibility: isHovered ? "visible" : "hidden",
+        }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="f-name" style={{ color: color }}>
+          <span>{firstName}</span>
+          <span>{lastName}</span>
+        </div>
+        <div className="description">
+          <span>{description}</span>
+        </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -21,30 +21,45 @@ const Characters = () => {
             firstName={"YATORA"}
             lastName={"YAGUCHI"}
             color={"#81d2e3"}
+            description={
+              "Second year high schooler. Fell in love with art after seeing Mori-senpai's painting. Yatora discovers his passion for painting and dedicates his remaining school years to it, aiming to pass the Tokyo University of the Arts(Geidai) entrance exam. "
+            }
           />
           <CharacterCard
             image={Ryuji}
             firstName={"RYUJI"}
             lastName={"AYUKAWA"}
             color={"#F3BED7"}
+            description={
+              "Goes by 'Yuka'. Yatora's classmate and fellow art club member who invited him to the art club. Yuka specializes in japanese art. Yatora thinks that Yuka is weird at first, but the two begin to bond after Yatora joins the art club."
+            }
           />
           <CharacterCard
             image={Yotosuke}
             firstName={"YOTOSUKE"}
             lastName={"TAKAHASHI"}
             color={"#B8DFBF"}
+            description={
+              "Goes to the same cram school as Yatora. Second year high schooler. Provokes Yatora with his genius art skills and antisocial personality. Takahashi and Yatora see each other as friendly rivals. "
+            }
           />
           <CharacterCard
             image={Haruka}
             firstName={"HARUKA"}
             lastName={"HASHIDA"}
             color={"#DAB4F1"}
+            description={
+              "Yatora's classmate. Goes to same cram school as Yatora. Yatora learns a lot of things from him. especially how to observe things. Yatora and Haruka can be seen as close friends."
+            }
           />
           <CharacterCard
             image={Maki}
             firstName={"MAKI"}
             lastName={"KUWANA"}
             color={"#E89093"}
+            description={
+              "Goes to same cram school as Yatora. Her parents are Geidai Alumni, and her older sister is a current student at Geidai as well as celebrity there. Maki deals with a lot of issues regarding her inferiority complex in comparison to her older sister."
+            }
           />
         </div>
       </div>
