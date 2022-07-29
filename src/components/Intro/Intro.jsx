c4975940c6705c4dc51a28dc6ee9e73d67b4e413
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-07-30 07:20:13 +1200

    responsive design

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React
+ from "react";
 import "./Intro.css";
 import MC from "../../img/Banner_Yatora.jfif";
 import Logo from "../../img/logo.png";
@@ -9,48 +10,47 @@ const Intro = () => {
   return (
     <div className="intro" id="Intro">
       <div className="banner">
-        <div className="yatora">
-          <img src={MC} className="mc" alt="" />
-          <motion.div className="i-title">
-            <motion.span
-              initial={{ x: "100vw" }}
-              animate={{ x: 0 }}
-              transition={{ duration: 1.5 }}
-            >
-              BLUE
-            </motion.span>
-            <motion.span
-              initial={{ x: "100vw" }}
-              animate={{ x: 0 }}
-              transition={{ duration: 1.5, delay: 0.3 }}
-            >
-              PERIOD
-            </motion.span>
-            <motion.img
-              src={Logo}
-              className="i-logo"
-              alt=""
-              initial={{ x: "100vw" }}
-              animate={{ x: 0 }}
-              transition={{ duration: 1.5, delay: 0.6 }}
-            />
-          </motion.div>
-          <div className="i-author">
-            <motion.span
-              initial={{ y: "100vw" }}
-              animate={{ y: 0 }}
-              transition={{ duration: 1.5, delay: 1 }}
-            >
-              written by
-            </motion.span>
-            <motion.span
-              initial={{ y: "100vw" }}
-              animate={{ y: 0 }}
-              transition={{ duration: 1.5, delay: 1.3 }}
-            >
-              TSUBASA YAMAGUCHI
-            </motion.span>
-          </div>
+        <img src={MC} className="banner-image" alt="" />
+        <motion.div className="i-title">
+          <motion.span
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.5 }}
+          >
+            BLUE
+          </motion.span>
+          <motion.span
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.5, delay: 0.3 }}
+          >
+            PERIOD
+          </motion.span>
+        </motion.div>
+        <motion.img
+          src={Logo}
+          className="i-logo"
+          alt=""
+          initial={{ x: "100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1.5, delay: 0.6 }}
+        />
+
+        <div className="i-author">
+          <motion.span
+            initial={{ y: "100vw" }}
+            animate={{ y: 0 }}
+            transition={{ duration: 1.5, delay: 1 }}
+          >
+            written by
+          </motion.span>
+          <motion.span
+            initial={{ y: "100vw" }}
+            animate={{ y: 0 }}
+            transition={{ duration: 1.5, delay: 1.3 }}
+          >
+            TSUBASA YAMAGUCHI
+          </motion.span>
         </div>
       </div>
       <div className="blue-line"></div>
